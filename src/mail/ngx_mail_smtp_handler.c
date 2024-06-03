4bc9fd4e2758e8338e9e528659d4300fad94aa82
Maxim Dounin <mdounin@mdounin.ru>
2024-06-03 18:03:05 +0300

    Mail: fixed EXTERNAL to be accepted only if enabled.
    As originally implemented in 6774:bcb107bb89cd, it wasn't possible to
    disable the EXTERNAL authentication method: it was always accepted
    (but not advertised unless enabled).  It is, however, believed that
    it is better to reject attempts to use the disabled method, hence in
    6869:b2915d99ee8d an attempt was made to address this.  This attempt
    was insufficient though: it was still possible to use the method as long
    as initial SASL response was used.
    
    With this patch both challenge-response and initial response forms are
    disabled.  Additionally, initial response handling for the PLAIN
    authentication is removed from ngx_mail_auth_parse(), for consistency
    and to don't provoke such bugs.
    (grafted from 6c54c8107baf00692cd82a0a8ed251af4b9224ad)

--- src/mail/ngx_mail_smtp_handler.c
+++ src/mail/ngx_mail_smtp_handler.c
@@ -701,6 +701,10 @@ ngx_mail_smtp_auth(ngx_mail_session_t *s, ngx_connection_t *c)
 
     case NGX_MAIL_AUTH_PLAIN:
 
+        if (s->args.nelts == 2) {
+            return ngx_mail_auth_plain(s, c, 1);
+        }
+
         ngx_str_set(&s->out, smtp_next);
         s->mail_state = ngx_smtp_auth_plain;
 
@@ -731,6 +735,10 @@ ngx_mail_smtp_auth(ngx_mail_session_t *s, ngx_connection_t *c)
 
         if (!(sscf->auth_methods & NGX_MAIL_AUTH_EXTERNAL_ENABLED)) {
             return NGX_MAIL_PARSE_INVALID_COMMAND;
+        }
+
+        if (s->args.nelts == 2) {
+            return ngx_mail_auth_external(s, c, 1);
         }
 
         ngx_str_set(&s->out, smtp_username);

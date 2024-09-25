42dc02c8539f0105261a98ec7293e3a4d0680566
Valentin Bartenev <vbart@wbsrv.ru>
2024-09-25 15:08:10 +0300

    Version bump to 1.8.0.

--- src/core/angie.h
+++ src/core/angie.h
@@ -8,8 +8,8 @@
 #define _ANGIE_H_INCLUDED_
 
 
-#define angie_version      1007000
-#define ANGIE_VERSION      "1.7.0"
+#define angie_version      1008000
+#define ANGIE_VERSION      "1.8.0"
 #define ANGIE_VER          "Angie/" ANGIE_VERSION
 
 #ifdef NGX_BUILD

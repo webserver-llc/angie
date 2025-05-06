1398ba3d77c8cc4823d4de9a6983417f55d0e6f9
Sergey Kandaurov <pluknet@nginx.com>
2025-05-06 18:57:01 +0400

    QUIC: logging missing mandatory TLS extensions only once.
    Previously, they might be logged on every add_handshake_data
    callback invocation when using OpenSSL compat layer and processing
    coalesced handshake messages.
    
    Further, the ALPN error message is adjusted to signal the missing
    extension.  Possible reasons were previously narrowed down with
    ebb6f7d65 changes in the ALPN callback that is invoked earlier in
    the handshake.
    (grafted from 45c02242fe62ee2969921e797007991ef36530c1)

--- src/event/quic/ngx_event_quic_ssl.c
+++ src/event/quic/ngx_event_quic_ssl.c
@@ -199,11 +199,15 @@ ngx_quic_add_handshake_data(ngx_ssl_conn_t *ssl_conn,
             SSL_get0_alpn_selected(ssl_conn, &alpn_data, &alpn_len);
 
             if (alpn_len == 0) {
-                qc->error = NGX_QUIC_ERR_CRYPTO(SSL_AD_NO_APPLICATION_PROTOCOL);
-                qc->error_reason = "unsupported protocol in ALPN extension";
+                if (qc->error == 0) {
+                    qc->error = NGX_QUIC_ERR_CRYPTO(
+                                               SSL_AD_NO_APPLICATION_PROTOCOL);
+                    qc->error_reason = "missing ALPN extension";
 
-                ngx_log_error(NGX_LOG_INFO, c->log, 0,
-                              "quic unsupported protocol in ALPN extension");
+                    ngx_log_error(NGX_LOG_INFO, c->log, 0,
+                                  "quic missing ALPN extension");
+                }
+
                 return 1;
             }
         }
@@ -247,11 +251,15 @@ ngx_quic_add_handshake_data(ngx_ssl_conn_t *ssl_conn,
             }
 
             /* RFC 9001, 8.2.  QUIC Transport Parameters Extension */
-            qc->error = NGX_QUIC_ERR_CRYPTO(SSL_AD_MISSING_EXTENSION);
-            qc->error_reason = "missing transport parameters";
 
-            ngx_log_error(NGX_LOG_INFO, c->log, 0,
-                          "missing transport parameters");
+            if (qc->error == 0) {
+                qc->error = NGX_QUIC_ERR_CRYPTO(SSL_AD_MISSING_EXTENSION);
+                qc->error_reason = "missing transport parameters";
+
+                ngx_log_error(NGX_LOG_INFO, c->log, 0,
+                              "missing transport parameters");
+            }
+
             return 1;
         }
     }

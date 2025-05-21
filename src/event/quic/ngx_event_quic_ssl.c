62395543a9c94ba2770dabb545e42439cb044bcd
Sergey Kandaurov <pluknet@nginx.com>
2025-05-21 19:55:31 +0400

    QUIC: logging of SSL library errors.
    Logging level for such errors, which should not normally happen,
    is changed to NGX_LOG_ALERT, and ngx_log_error() is replaced with
    ngx_ssl_error() for consistency with the rest of the code.
    (grafted from 05091cb56468bdc21521ca096f26f5e68e82e2c5)

--- src/event/quic/ngx_event_quic_ssl.c
+++ src/event/quic/ngx_event_quic_ssl.c
@@ -482,7 +482,7 @@ ngx_quic_crypto_input(ngx_connection_t *c, ngx_chain_t *data,
         b = cl->buf;
 
         if (!SSL_provide_quic_data(ssl_conn, level, b->pos, b->last - b->pos)) {
-            ngx_ssl_error(NGX_LOG_INFO, c->log, 0,
+            ngx_ssl_error(NGX_LOG_ALERT, c->log, 0,
                           "SSL_provide_quic_data() failed");
             return NGX_ERROR;
         }
@@ -616,7 +616,7 @@ ngx_quic_init_connection(ngx_connection_t *c)
     }
 
     if (SSL_set_quic_method(ssl_conn, &quic_method) == 0) {
-        ngx_log_error(NGX_LOG_INFO, c->log, 0,
+        ngx_ssl_error(NGX_LOG_ALERT, c->log, 0,
                       "quic SSL_set_quic_method() failed");
         return NGX_ERROR;
     }
@@ -663,14 +663,14 @@ ngx_quic_init_connection(ngx_connection_t *c)
 #endif
 
     if (SSL_set_quic_transport_params(ssl_conn, p, len) == 0) {
-        ngx_log_error(NGX_LOG_INFO, c->log, 0,
+        ngx_log_error(NGX_LOG_ALERT, c->log, 0,
                       "quic SSL_set_quic_transport_params() failed");
         return NGX_ERROR;
     }
 
 #if defined(OPENSSL_IS_BORINGSSL) || defined(OPENSSL_IS_AWSLC)
     if (SSL_set_quic_early_data_context(ssl_conn, p, clen) == 0) {
-        ngx_log_error(NGX_LOG_INFO, c->log, 0,
+        ngx_log_error(NGX_LOG_ALERT, c->log, 0,
                       "quic SSL_set_quic_early_data_context() failed");
         return NGX_ERROR;
     }

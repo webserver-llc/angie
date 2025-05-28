7f43acba00bb1202873a4637faa78cc6e7b408ff
Valentin Bartenev <vbart@wbsrv.ru>
2025-05-28 20:23:32 +0300

    Version bump to 1.9.1.
    --HG--
    branch : 1.9

--- src/core/angie.h
+++ src/core/angie.h
@@ -8,8 +8,8 @@
 #define _ANGIE_H_INCLUDED_
 
 
-#define angie_version      1009000
-#define ANGIE_VERSION      "1.9.0"
+#define angie_version      1009001
+#define ANGIE_VERSION      "1.9.1"
 #define ANGIE_VER          "Angie/" ANGIE_VERSION
 
 #ifdef NGX_BUILD

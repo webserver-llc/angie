b5c59d36a71b64612ecb722654edb9f8baaeb3ff
Rodion Nikolov <nikolov@wbsrv.ru>
2025-06-26 18:37:43 +0300

    Core: added a function for processing first object in JSON.
    The ngx_json_parse_first_object() function allows parsing
    first object in JSON and provides a way to obtain a pointer
    to the end of a valid JSON object in the string.

--- src/core/ngx_json.c
+++ src/core/ngx_json.c
@@ -58,8 +58,8 @@ static void *ngx_json_parse_error(ngx_json_parse_error_t *error, u_char *pos,
 
 
 ngx_data_item_t *
-ngx_json_parse(u_char *start, u_char *end, ngx_pool_t *pool,
-    ngx_json_parse_error_t *error)
+ngx_json_parse_first_object(u_char *start, u_char *end, u_char **last,
+    ngx_pool_t *pool, ngx_json_parse_error_t *error)
 {
     u_char           *p;
     ngx_data_item_t  *item;
@@ -82,8 +82,23 @@ ngx_json_parse(u_char *start, u_char *end, ngx_pool_t *pool,
 
     p = ngx_json_skip_space(p, end);
 
-    if (p != end) {
-        return ngx_json_parse_error_const(error, p,
+    *last = p;
+
+    return item;
+}
+
+
+ngx_data_item_t *
+ngx_json_parse(u_char *start, u_char *end, ngx_pool_t *pool,
+    ngx_json_parse_error_t *error)
+{
+    u_char           *last;
+    ngx_data_item_t  *item;
+
+    item = ngx_json_parse_first_object(start, end, &last, pool, error);
+
+    if (last != end) {
+        return ngx_json_parse_error_const(error, last,
             "Unexpected character after the end of a valid JSON value."
         );
     }
--- src/core/ngx_json.h
+++ src/core/ngx_json.h
@@ -24,5 +24,8 @@ ngx_buf_t *ngx_json_render(ngx_pool_t *pool, ngx_data_item_t *item,
 ngx_data_item_t *ngx_json_parse(u_char *start, u_char *end, ngx_pool_t *pool,
     ngx_json_parse_error_t *error);
 
+ngx_data_item_t *ngx_json_parse_first_object(u_char *start, u_char *end,
+    u_char **last, ngx_pool_t *pool, ngx_json_parse_error_t *error);
+
 
 #endif /* _NGX_JSON_H_INCLUDED_ */

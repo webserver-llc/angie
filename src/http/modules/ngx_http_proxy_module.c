535aab15a8857a8f2e07462a9d8f6ad9bbaa0e91
Sergey Kandaurov <pluknet@nginx.com>
2025-12-17 18:12:28 +0300

    Proxy: fixed segfault in URI change.
    If request URI was shorter than location prefix, as after replacement
    with try_files, location length was used to copy the remaining URI part
    leading to buffer overread.
    
    The fix is to replace full request URI in this case.  In the following
    configuration, request "/123" is changed to "/" when sent to backend.
    
        location /1234 {
            try_files /123 =404;
            proxy_pass http://127.0.0.1:8080/;
        }
    
    Closes #983 on GitHub.
    (grafted from dfd6b9504332d5f370d382f5e89fb66968f2752e)

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -1517,7 +1517,8 @@ ngx_http_proxy_create_key(ngx_http_request_t *r)
         return NGX_OK;
     }
 
-    loc_len = (r->valid_location && ctx->vars.uri.len) ? plcf->location.len : 0;
+    loc_len = (r->valid_location && ctx->vars.uri.len)
+              ? ngx_min(plcf->location.len, r->uri.len) : 0;
 
     if (r->quoted_uri || r->internal) {
         escape = 2 * ngx_escape_uri(NULL, r->uri.data + loc_len,
@@ -1627,8 +1628,8 @@ ngx_http_proxy_create_request(ngx_http_request_t *r)
         uri_len = r->unparsed_uri.len;
 
     } else {
-        loc_len = (r->valid_location && ctx->vars.uri.len) ?
-                      plcf->location.len : 0;
+        loc_len = (r->valid_location && ctx->vars.uri.len)
+                  ? ngx_min(plcf->location.len, r->uri.len) : 0;
 
         if (r->quoted_uri || r->internal) {
             escape = 2 * ngx_escape_uri(NULL, r->uri.data + loc_len,

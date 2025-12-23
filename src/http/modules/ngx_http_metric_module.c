72a06d8a8a41a9781f20c9e826d14588931c11f0
Daniel Savitskiy <savitskiy@wbsrv.ru>
2025-12-23 16:57:36 +0300

    Metric: allowed the "metric" directive in the "http" and "server" contexts.

--- src/http/modules/ngx_http_metric_module.c
+++ src/http/modules/ngx_http_metric_module.c
@@ -341,7 +341,7 @@ static ngx_command_t  ngx_http_metric_commands[] = {
       NULL },
 
     { ngx_string("metric"),
-      NGX_HTTP_LOC_CONF|NGX_CONF_TAKE23,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE23,
       ngx_http_metric,
       NGX_HTTP_LOC_CONF_OFFSET,
       0,

a8c570f44ca3c6a4dc3a65d7968595d58d8103ef
Vladimir Khomutov <vl@wbsrv.ru>
2026-01-22 11:30:33 +0300

    Tests: added JSON formatter.
    To facilitate processing of test results, an additional test report
    in JSON format is produced by TAP formatter if requested.
    
    Formatter is disabled by default, pass REPORT=yes option to tests makefile
    to get report in a testreport-<datetime>.json file in the current directory.
    
    The console output is produced TAP::Formatter::File. The stdout and stderr
    of each test are captured while running and added to the test report.
    
    Verbosity can be controlled by the TAP_FORMATTER_VERBOSITY environment variable.
    
    The report contains full statistics on executed testcases, their statuses
    and output of all testcases. The per-testcase output is clean and not
    intermixed with other testcases even with parallel execution.

---
+++ tests/lib/Test/Formatter/JSON.pm
@@ -0,0 +1,295 @@
+#!/usr/bin/perl
+
+# (C) 2026 Web Server LLC
+
+# Test Formatter class: uses TAP::Formatter::Console to provide console
+# output and additionally generates JSON report using the template
+
+###############################################################################
+
+package Test::Formatter::JSON;
+
+use strict;
+use warnings;
+
+use IO::File;
+use File::Temp qw(tempdir);
+use File::Spec::Functions qw(catdir);
+use Test::Formatter::TestSession;
+use TAP::Formatter::File;
+use POSIX 'strftime';
+use JSON;
+
+use base qw(TAP::Base);
+
+BEGIN {
+	my %PROPS= (
+		verbosity          => sub { shift; shift; },
+		stdout             => sub { shift; shift; },
+		output_fh          => sub { shift; shift; },
+		session_class      => sub { shift; shift; },
+		template_processor => sub { shift; shift; },
+		template           => sub { shift; shift; },
+		sessions           => sub { shift; shift; },
+		tests              => sub { shift; shift; },
+		console            => sub { shift; shift; }
+	);
+	__PACKAGE__->mk_methods(keys %PROPS);
+}
+
+sub _initialize {
+	my ($class, $args) = @_;
+
+	$args ||= {};
+	my $self = $class->SUPER::_initialize($args);
+
+	# may be overridden by prove opts
+	$self->verbosity(0);
+	$self->session_class('Test::Formatter::TestSession');
+	$self->sessions([]);
+
+	my $report_fn = strftime 'testreport-%Y%m%d_%H%M%S.json', localtime;
+
+	$self->output_file($report_fn);
+
+	# create a default formatter for display of text results
+	# TODO: use Tap::Formatter::Console when run in real console
+	# and Tap::Formatter::File when running redirected to file
+	$self->console(TAP::Formatter::File->new($args));
+
+	# consider args correct and pass them directly to base class
+	foreach my $key (keys %$args) {
+		$self->$key($args->{$key}) if ($self->can($key));
+	}
+
+	# quiet:  -1 // prove -q
+	# normal:  0 // prove
+	# verbose: 1 // prove - v
+	my $fmt_verb = $ENV{TAP_FORMATTER_VERBOSITY} // 1;
+	$self->verbosity($fmt_verb);
+
+	return $self;
+}
+
+
+sub output_file {
+	my ($self, $file) = @_;
+	my $fh = IO::File->new( $file, 'w' ) or die "Error opening '$file' $!";
+	$self->output_fh( $fh );
+}
+
+
+sub prepare {
+	my ($self, @tests) = @_;
+	$self->tests([@tests]);
+	$self->console->prepare(@tests);
+}
+
+
+sub open_test {
+	my ($self, $test, $parser) = @_;
+
+	my $console_session = $self->console->open_test($test, $parser);
+
+	my $session = $self->session_class->new({ test => $test,
+	                                          parser => $parser,
+	                                          formatter => $self,
+	                           console_session => $console_session});
+
+	push @{ $self->sessions }, $session;
+
+	return $session;
+}
+
+
+sub summary {
+	my ($self, $agg) = @_;
+
+	# normal console report
+	$self->console->summary($agg);
+
+	my $r = {
+	     tests => [],
+	     start_time => '?',
+	     end_time => '?',
+	     elapsed_time => $agg->elapsed_timestr,
+	};
+
+	for my $key (qw(total has_errors has_problems failed parse_errors passed
+	             skipped todo todo_passed wait exit))
+	{
+		$r->{$key} = $agg->$key;
+	}
+
+	$r->{num_files} = scalar @{ $self->sessions };
+
+	my $total_time = 0;
+	foreach my $s (@{ $self->sessions }) {
+		my $sr = $s->session_report;
+		push @{$r->{tests}}, $sr;
+		$total_time += $sr->{elapsed_time} || 0;
+	}
+	$r->{total_time} = $total_time;
+
+	my $out = create_report($r, $self->verbosity);
+
+	my $j = JSON->new->canonical(1);
+	my $res = $j->pretty->utf8->encode($out);
+
+	print { $self->output_fh } ${ \$res };
+	close($self->output_fh);
+
+	return $self;
+}
+
+
+sub _output {
+	my $self = shift;
+	$self->console->_output(@_);
+}
+
+
+sub create_report {
+	my ($r, $verbosity) = @_;
+
+	my %res = ();
+
+	# status
+
+	if ($r->{has_errors}) {
+		$res{status} = 'failed';
+
+	} else {
+		$res{status} = 'passed';
+	}
+
+	$res{exit_status} = $r->{exit};
+	$res{wait_status} = $r->{wait};
+
+	# counters
+
+	$res{parse_errors} = $r->{parse_errors};
+
+	$res{tc_total} = $r->{total} + $r->{skipped};
+	$res{tc_passed} = $r->{passed};
+	$res{tc_failed} = $r->{failed};
+	$res{tc_skipped} = $r->{skipped};
+	$res{tc_todo} = $r->{todo};
+	$res{tc_todo_passed} = $r->{todo_passed};
+
+	# execution time
+
+	$res{elapsed} = $r->{elapsed_time};
+	$res{total_time} = $r->{total_time};
+
+	my $files_passed = 0;
+	my $files_failed = 0;
+	my $files_skipped = 0;
+
+	my %tests = ();
+
+	for my $test (@{$r->{tests}}) {
+		my %tres = ();
+		my $tname = ($test->{test_base});
+
+		if ($test->{skip_all}) {
+			$files_skipped++;
+		} elsif ($test->{test_status} eq 'passed' && $test->{tests_run} > 0) {
+			$files_passed++;
+		} else {
+			$files_failed++;
+		}
+
+		$tres{exit_status} = $test->{exit};
+		$tres{wait_status} = $test->{wait};
+		$tres{status} = $test->{test_status};
+		$tres{todo_passed} = $test->{todo_passed};
+		$tres{problems} = $test->{has_problems};
+		$tres{parse_errors} = $test->{num_parse_errors};
+		$tres{skip_all} = $test->{skip_all};
+		$tres{planned} = $test->{tests_planned};
+
+		$tres{run} = $test->{tests_run};
+		$tres{passed} = $test->{passed};
+		$tres{failed} = $test->{failed};
+		$tres{todo} = $test->{todo};
+		$tres{skipped} = $test->{skipped};
+		$tres{elapsed} = $test->{elapsed_time};
+		$tres{time_wall} = $test->{time_wall};
+		$tres{time_user} = $test->{time_user};
+		$tres{time_user_child} = $test->{time_user_child};
+		$tres{time_user_total} = $test->{time_user_total};
+		$tres{time_system} = $test->{time_system};
+		$tres{time_system_child} = $test->{time_system_child};
+		$tres{time_system_total} = $test->{time_system_total};
+		$tres{time_process} = $test->{time_process};
+
+		if ($verbosity > 0) {
+			my @stdout;
+			my @stderr;
+
+			for my $line (@{$test->{xresults}}) {
+				push @stdout, $line->{raw};
+			}
+
+			for my $line (@{$test->{err_lines}}) {
+				push @stderr, $line;
+			}
+
+			$tres{stdout} = \@stdout;
+			if (scalar @stderr) {
+				$tres{stderr} = \@stderr;
+			}
+		}
+
+		$tres{failed_tests} = $test->{failed_tests};
+
+		if ($verbosity > -1) {
+			my %stderr = ();
+
+			for my $tnum (keys %{$test->{tc_errors}}) {
+
+				my @tc_stderr;
+				for my $line (@{$test->{tc_errors}{$tnum}}) {
+					push @tc_stderr, $line;
+				}
+
+				$stderr{$tnum} = \@tc_stderr;
+			}
+
+			if (scalar keys %stderr) {
+				$tres{tc_stderr} = \%stderr;
+			}
+
+			my %misc= ();
+
+			for my $tnum (keys %{$test->{tc_misc}}) {
+
+				my @tc_misc;
+				for my $line (@{$test->{tc_misc}{$tnum}}) {
+					push @tc_misc, $line;
+				}
+
+				$misc{$tnum} = \@tc_misc;
+			}
+
+			if (scalar keys %misc) {
+				$tres{tc_misc} = \%misc;
+			}
+		}
+
+		$tests{$tname} = \%tres;
+	}
+
+	$res{files_total} = $r->{num_files};
+	$res{files_passed} = $files_passed;
+	$res{files_failed} = $files_failed;
+	$res{files_skipped} = $files_skipped;
+
+	$res{tests} = \%tests;
+
+	return \%res;
+}
+
+1;
+
---
+++ tests/lib/Test/Formatter/TestSession.pm
@@ -0,0 +1,261 @@
+#!/usr/bin/perl
+
+# (C) 2026 Web Server LLC
+
+# Test Session class - results of a single test file execution
+
+###############################################################################
+
+package Test::Formatter::TestSession;
+
+use strict;
+use warnings;
+
+use File::Basename;
+use File::Temp qw /tempfile/ ;
+use Test::Utils qw/trim :re/;
+use Time::HiRes qw(time);
+
+use base qw( TAP::Base );
+
+BEGIN {
+	my %PROPS= (
+		test            => sub { shift; shift; },
+		formatter       => sub { shift; shift; },
+		parser          => sub { shift; shift; },
+		results         => sub { shift; shift; },
+		xresults        => sub { shift; shift; },
+		tc_errors       => sub { shift; shift; },
+		tc_misc         => sub { shift; shift; },
+		meta            => sub { shift; shift; },
+		closed          => sub { shift; shift; },
+		tmp_fh          => sub { shift; shift; },
+		tmp_fn          => sub { shift; shift; },
+		orig_stderr     => sub { shift; shift; },
+		err_lines       => sub { shift; shift; },
+		curr_tc_num     => sub { shift; shift; },
+		console_session => sub { shift; shift; },
+		start_wall      => sub { shift; shift; },
+		start_user      => sub { shift; shift; },
+		start_cuser     => sub { shift; shift; },
+		start_system    => sub { shift; shift; },
+		start_csystem   => sub { shift; shift; },
+	);
+	__PACKAGE__->mk_methods(keys %PROPS);
+}
+
+
+sub _initialize {
+	my ($self, $args) = @_;
+
+	$args ||= {};
+	$self->SUPER::_initialize($args);
+
+	$self->results([]);
+	$self->xresults([]);
+	$self->tc_errors({});
+	$self->tc_misc({});
+	$self->meta({});
+	$self->closed(0);
+	$self->curr_tc_num(0);
+
+	foreach my $arg (qw(test parser formatter console_session)) {
+		$self->$arg($args->{$arg}) if defined $args->{$arg};
+	}
+
+	$self->grab_stderr();
+
+	$self->start_wall(time());
+
+	my ($user, $system, $cuser, $csystem) = times;
+
+	$self->start_user($user);
+	$self->start_cuser($cuser);
+	$self->start_system($system);
+	$self->start_csystem($csystem);
+
+	return $self;
+}
+
+
+sub result {
+	my ($self, $result) = @_;
+
+	# dump and grab up-to-date stderr
+	$self->restore_stderr($result);
+	# start the next test if any
+	$self->grab_stderr();
+
+	$self->console_session->result($result);
+
+	if ($result->is_test) {
+
+		$result->{test_status}  = $result->has_todo ? 'todo-' : '';
+		$result->{test_status} .= $result->has_skip ? 'skip-' : '';
+		$result->{test_status} .= $result->is_actual_ok ? 'ok' : 'not-ok';
+
+		if ($result->has_skip) {
+			$self->meta->{skipped}++;
+		}
+
+		if ($result->is_ok) {
+			$self->meta->{passed}++;
+		}
+
+		if ($result->has_todo) {
+			if ($result->is_actual_ok) {
+				$result->{todo_passed} = 1;
+			}
+			$self->meta->{todo}++;
+		}
+
+		$self->curr_tc_num($self->curr_tc_num + 1);
+	}
+
+	push @{ $self->results }, $result;
+	push @{ $self->xresults }, \%$result;
+}
+
+
+sub close_test {
+	my ($self, @args) = @_;
+
+	my $wall = time();
+	my ($user, $system, $cuser, $csystem) = times;
+
+
+	$self->{time_wall} = $wall - $self->start_wall;
+	$self->{time_user} = ($user - $self->start_user);
+	$self->{time_user_child} = ($cuser - $self->start_cuser);
+	$self->{time_user_total} =  $self->{time_user} + $self->{time_user_child};
+	$self->{time_system} = $system - $self->start_system;
+	$self->{time_system_child} = ($csystem - $self->start_csystem);
+	$self->{time_system_total} = $self->{time_system} + $self->{time_system_child};
+	$self->{time_process} = $self->{time_system_total} + $self->{time_user_total};
+
+	$self->restore_stderr();
+
+	$self->console_session->close_test(@args);
+
+	$self->closed(1);
+}
+
+
+sub session_report {
+	my ($self) = @_;
+
+	my $p = $self->parser;
+	my $r = {
+		test => $self->test,
+		test_base => basename($self->test),
+		xresults => $self->xresults,
+		tc_errors => $self->tc_errors,
+		tc_misc => $self->tc_misc,
+		err_lines => $self->err_lines,
+	};
+
+	for my $key (qw(tests_planned tests_run start_time end_time skip_all
+				skipped has_problems passed failed todo todo_passed
+				actual_passed actual_failed wait exit))
+	{
+		$r->{$key} = $p->$key;
+    }
+
+	$r->{num_parse_errors} = scalar $p->parse_errors;
+	$r->{parse_errors} = [ $p->parse_errors ];
+	$r->{passed_tests} = [ $p->passed ];
+	$r->{failed_tests} = [ $p->failed ];
+
+	$r->{test_status} = $r->{has_problems} ? 'failed' : 'passed';
+	$r->{elapsed_time} = $r->{end_time} - $r->{start_time};
+
+	$r->{time_wall} = $self->{time_wall};
+	$r->{time_user} = $self->{time_user};
+	$r->{time_user_child} = $self->{time_user_child};
+	$r->{time_user_total} = $self->{time_user_total};
+	$r->{time_system} = $self->{time_system};
+	$r->{time_system_child} = $self->{time_system_child};
+	$r->{time_system_total} = $self->{time_system_total};
+	$r->{time_process} = $self->{time_process};
+
+	return $r;
+}
+
+
+sub grab_stderr {
+	my ($self) = @_;
+
+	# TODO: pass $t->testdir() here and use per-test directory
+
+	# capture stderr since now into this file
+	my ($fh, $fn) = tempfile('angie-test-stderr-XXXXXXXX', TMPDIR => 1);
+	$self->tmp_fh($fh);
+	$self->tmp_fn($fn);
+
+	open my $old_stderr, '>&STDERR' or die "Cannot save STDERR: $!";
+
+	$self->orig_stderr($old_stderr);
+
+	open STDERR, '>&', $fh or die "Cannot redirect STDERR: $!";
+}
+
+
+sub restore_stderr {
+	my ($self, $result) = @_;
+
+	# save stderr of this test
+	close($self->tmp_fh);
+
+	my $lines = get_lines($self->tmp_fn);
+
+	if (defined $result) {
+		# per-test-case stderr
+		my $len = scalar @$lines;
+
+		if ($len != 0) {
+			# do not push empty out (most of it)
+			# object index is testcase number
+			if ($result->is_test) {
+				$self->tc_errors->{$self->curr_tc_num} = $lines;
+			} else {
+				$self->tc_misc->{$self->curr_tc_num} = $lines;
+			}
+		}
+
+	} else {
+		# global, per-test-file stderr, if any
+		$self->err_lines($lines);
+	}
+
+	unlink $self->tmp_fn or warn "Cannot remove temporary file: $!";
+
+	# restore stderr
+	open STDERR, '>&', $self->orig_stderr or die "Cannot restore STDERR: $!";
+
+	# dump captured stderr for this test file
+	for my $line (@$lines) {
+		print STDERR "$line\n";
+	}
+}
+
+
+sub get_lines {
+	my ($file) = @_;
+
+	my $fh;
+
+	open $fh, '<', $file or do {
+		warn "Cannot open $file: $!";
+		return [];
+	};
+
+	my @lines;
+	for my $line (<$fh>) {
+		$line = trim($line);
+		push @lines, $line;
+	}
+
+	return \@lines;
+}
+
+1;

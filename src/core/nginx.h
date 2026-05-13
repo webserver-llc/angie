c7089ff2fb1e6ec602d94e96590cb2620aadb774
Andrew Clayton <a.clayton@nginx.com>
2026-05-13 20:18:39 +0100

    Version bump (grafted from 7e703d2807edc1aeb97c4e597692f7c11c188dfd)

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -10,8 +10,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1031000
-#define NGINX_VERSION      "1.31.0"
+#define nginx_version      1031001
+#define NGINX_VERSION      "1.31.1"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VER_BUILD    NGINX_VER

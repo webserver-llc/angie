c2b3e063d6ef5e5a5c7bf5364df744f1f39ac990
Valentin Bartenev <vbart@wbsrv.ru>
2026-05-28 14:42:14 +0300

    Version bump to 1.11.7.
    --HG--
    branch : 1.11

--- src/core/angie.h
+++ src/core/angie.h
@@ -8,8 +8,8 @@
 #define _ANGIE_H_INCLUDED_
 
 
-#define angie_version      1011006
-#define ANGIE_VERSION      "1.11.6"
+#define angie_version      1011007
+#define ANGIE_VERSION      "1.11.7"
 #define ANGIE_VER          "Angie/" ANGIE_VERSION
 
 #ifdef NGX_BUILD
